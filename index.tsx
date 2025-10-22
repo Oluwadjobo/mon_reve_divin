0282b43e371ddd1966e2d108e189ff0b1530d1a3
Oluwadjobo <gerard@webcoomgroup.net>
2025-10-22 12:39:04 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,27 @@
 
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import './index.css';
+//import React from 'react';
+//import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
+/*const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);*/
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+// Avec UMD React 18, createRoot est sur ReactDOM global
+const root = (ReactDOM as any).createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
