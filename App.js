ad9459de122962bf2c44aa329785c4bc4fd04ebb
Oluwadjobo <gerard@webcoomgroup.net>
2025-10-22 13:14:58 +0100

    Update and rename App.tsx to App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-//import React from 'react';
+import React from 'react';
 import { CartProvider } from './context/CartContext';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
